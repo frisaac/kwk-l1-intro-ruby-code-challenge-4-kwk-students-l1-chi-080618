e9e5e4e4abec0abad027f49cc3707909865be8f3
frisaac <frisaac@stu.naperville203.org>
2018-08-08 21:26:17 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,13 @@
 #write out your code here
 
 def least_coins(cents)
-
 #Code your answer here!
-
+  quarters = cents/25
+  dimes = (cents%25)/10
+  nickels = ((cents%25)%10)/5
+  pennies = (((cents%25)%10)%5)/1
+  return quarters
+  return dimes
+  return nickels
+  return pennies
 end
